0986180bbdccf53d2a8bc02257e4a6cdb912e603
= <=>
2021-10-13 09:39:11 +0300

    Added main for models

--- hcbot/settings.py
+++ hcbot/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_tgbot',
-    'hidden_chat_bot'
+    'hidden_chat_bot',
+    'main'
 ]
 
 MIDDLEWARE = [
